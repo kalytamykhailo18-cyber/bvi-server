fecd477ce0b29cd9b9affca58410722e8b2d0da1
kalytamykhailo18-cyber <kalytamykhailo18@gmail.com>
2025-11-17 07:35:00 -0500

    fixed port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const compression = require('compression');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
